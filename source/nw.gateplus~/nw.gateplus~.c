396a2420a86157ac7387f6e31622deeeb74edf39
nwolek <nw@nathanwolek.com>
2015-05-11 09:56:56 -0400

    gateplus: drafting dsp64() and perform64() methods

--- source/nw.gateplus~/nw.gateplus~.c
+++ source/nw.gateplus~/nw.gateplus~.c
@@ -47,6 +47,8 @@ typedef struct _gateplus
 
 /* method definitions for this object */
 void *gateplus_new(long outlets);
+void gateplus_dsp64(t_gateplus *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags);
+void gateplus_perform64(t_gateplus *x, t_object *dsp64, double **ins, long numins, double **outs,long numouts, long vectorsize, long flags, void *userparam);
 
 
 /********************************************************************************
@@ -64,6 +66,9 @@ int C74_EXPORT main(void)
     c = class_new(OBJECT_NAME, (method)gateplus_new, (method)dsp_free,
                   (short)sizeof(t_gateplus), 0L, 1, 0);
     class_dspinit(c); // add standard functions to class
+    
+    /* bind method "gateplus_dsp64" to the dsp64 message */
+    class_addmethod(c, (method)gateplus_dsp64, "dsp64", A_CANT, 0);
     
     class_register(CLASS_BOX, c); // register the class w max
     gateplus_class = c;
@@ -105,4 +110,90 @@ void *gateplus_new(long outlets)
     return (x);
 }
 
-                  
+/********************************************************************************
+ void gateplus_dsp64()
+ 
+ inputs:     x		-- pointer to this object
+ dsp64		-- signal chain to which object belongs
+ count	-- array detailing number of signals attached to each inlet
+ samplerate -- number of samples per second
+ maxvectorsize -- sample frames per vector of audio
+ flags --
+ description:	called when 64 bit DSP call chain is built; adds object to signal flow
+ returns:		nothing
+ ********************************************************************************/
+void gateplus_dsp64(t_gateplus *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags)
+{
+#ifdef DEBUG
+    post("%s: adding 64 bit perform method", OBJECT_NAME);
+#endif /* DEBUG */
+    
+    if (count[1]) { // if signal input is connected
+        #ifdef DEBUG
+            post("%s: output is being computed", OBJECT_NAME);
+        #endif /* DEBUG */
+        dsp_add64(dsp64, (t_object*)x, (t_perfroutine64)gateplus_perform64, 0, NULL);
+    } else {
+        #ifdef DEBUG
+            post("%s: no output computed", OBJECT_NAME);
+        #endif /* DEBUG */
+    }
+    
+}
+
+/********************************************************************************
+ void *gateplus_perform64()
+ 
+ inputs:	x		--
+ dsp64   --
+ ins     --
+ numins  --
+ outs    --
+ numouts --
+ vectorsize --
+ flags   --
+ userparam  --
+ description:	called at interrupt level to compute object's output at 64-bit,
+ writes zeros to every outlet
+ returns:		nothing
+ ********************************************************************************/
+void gateplus_perform64(t_gateplus *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long vectorsize, long flags, void *userparam)
+{
+    // local vars outlets and inlets
+    t_double *in_ctrl = ins[0];
+    t_double *in_signal = ins[1];
+    t_double *out_signal = outs[0];
+    
+    // local vars for object vars and while loop
+    long n;
+    short g_stage;
+    double lc_in, ls_in;
+    
+    // check to make sure object is enabled
+    if (x->x_obj.z_disabled) goto out; // if not, skip ahead
+    
+    // assign values to local vars
+    g_stage = x->gate_stage;
+    lc_in = x->last_ctrl_in;
+    ls_in = x->last_sig_in;
+    
+    n = vectorsize;
+    while (n--) {
+        
+        // temporarily write zeros
+        *out_signal = 0.;
+        
+        // advance pointers
+        ++in_ctrl, ++in_signal, ++out_signal;
+    }
+    
+    // update global vars
+    x->gate_stage = g_stage;
+    x->last_ctrl_in = lc_in;
+    x->last_sig_in = ls_in;
+    
+out:
+    return;
+    
+}
+
