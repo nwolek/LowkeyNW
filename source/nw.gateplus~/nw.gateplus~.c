e60cae56b2c7136800ec502176a6fe62d7c3bf23
nwolek <nw@nathanwolek.com>
2015-05-11 09:02:49 -0400

    gateplus: basic object elements in place, compiles but has no functionality yet.

--- source/nw.gateplus~/nw.gateplus~.c
+++ source/nw.gateplus~/nw.gateplus~.c
@@ -34,5 +34,68 @@ static t_class *gateplus_class;		// required global pointing to this class
 
 typedef struct _gateplus
 {
-
+    t_pxobject x_obj;					// <--
+    
+    // current gate info
+    short gate_stage;       // see flags in header
+    
+    //history
+    double last_ctrl_in;
+    double last_sig_in;
+    
 } t_gateplus;
+
+/* method definitions for this object */
+void *gateplus_new(long outlets);
+
+
+/********************************************************************************
+ void main(void)
+ 
+ inputs:			nothing
+ description:	called the first time the object is used in MAX environment;
+ defines inlets, outlets and accepted messages
+ returns:		int
+ ********************************************************************************/
+int C74_EXPORT main(void)
+{
+    t_class *c;
+    
+    c = class_new(OBJECT_NAME, (method)gateplus_new, (method)dsp_free,
+                  (short)sizeof(t_gateplus), 0L, 1, 0);
+    class_dspinit(c); // add standard functions to class
+    
+    class_register(CLASS_BOX, c); // register the class w max
+    gateplus_class = c;
+    
+    #ifndef DEBUG
+        
+    #endif /* DEBUG */
+}
+
+/********************************************************************************
+ void *gateplus_new(long outlets)
+ 
+ inputs:			outlets		-- NOT USED YET`
+ description:	called for each new instance of object in the MAX environment;
+ defines inlets and outlets; sets argument for number of outlets
+ returns:		nothing
+ ********************************************************************************/
+void *gateplus_new(long outlets)
+{
+    t_gateplus *x = (t_gateplus *) object_alloc((t_class*) gateplus_class);
+    
+    dsp_setup((t_pxobject *)x, 2);					// two inlets
+    outlet_new((t_pxobject *)x, "signal");			// one outlet
+    
+    x->x_obj.z_misc = Z_NO_INPLACE;
+    
+    #ifdef DEBUG
+        post("%s: new function was called", OBJECT_NAME);
+    #endif /* DEBUG */
+    
+    /* return a pointer to the new object */
+    return (x);
+}
+
+                  
