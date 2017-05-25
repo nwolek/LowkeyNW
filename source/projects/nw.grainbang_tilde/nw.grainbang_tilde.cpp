a83ebd49f0d6da353666ecc4433b865489441a48
Nathan Wolek <nathanwolek@LowkeyMacBook.local>
2017-05-25 19:44:59 -0400

    more changes to support stereo buffers with grainbang. working without mono copy to outlet 2. issue #18

--- source/projects/nw.grainbang_tilde/nw.grainbang_tilde.cpp
+++ source/projects/nw.grainbang_tilde/nw.grainbang_tilde.cpp
@@ -474,10 +474,14 @@ void grainbang_perform64(t_grainbang *x, t_object *dsp64, double **ins, long num
         // get value from the snd buffer samples
         if (interp_s == INTERP_ON) {
             snd_out = mcLinearInterp(tab_s, temp_index_int_times_chan, temp_index_frac, size_s, chan_s);
-            snd_out2 = 0.;
+            snd_out2 = (chan_s == 2) ?
+                mcLinearInterp(tab_s, temp_index_int_times_chan + 1, temp_index_frac, size_s, chan_s) :
+                0.;
         } else {	// if INTERP_OFF
             snd_out = tab_s[temp_index_int_times_chan];
-            snd_out2 = 0.;
+            snd_out2 = (chan_s == 2) ?
+                tab_s[temp_index_int_times_chan + 1] :
+                0.;
         }
         
         // OUTLETS
